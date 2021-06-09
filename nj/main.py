18fea1b4cfe598455fdeda03fb229c2887ed17b0
maayanpolitzer <maayanp163@gmail.com>
2021-06-09 21:26:52 +0300

    update

--- nj/main.py
+++ nj/main.py
@@ -1,48 +1,5 @@
 def get_minimum_connections(matrix):
-    keys = {}
-    groups = 0
-    row = 0
-    while row < len(matrix):
-        col = row + 1
-        while col < len(matrix[row]):
-            if matrix[row][col]:
-                if row in keys:
-                    keys[col] = row
-                elif col in keys:
-                    keys[row] = keys[col]
-                else:
-                    groups += 1
-                    keys[row] = row
-                    keys[col] = row
-            col += 1
-        row += 1
-    return groups - 1
 
-
-def bfs(matrix):
-    groups = set([])
-    visited = [[False for i in range(len(matrix))] for i in range(len(matrix))]
-    queue = []
-    k = 0
-    while k < len(matrix):
-        queue.append((k, 0))
-        while len(queue) > 0:
-            cell = queue.pop(0)
-            row = cell[0]
-            col = cell[1]
-            while col < len(matrix[row]):
-                if matrix[row][col] and not visited[row][col]:
-                    print(f"row: {row}, col: {col}")
-                    groups.add(row)
-                    queue.append((row, col))
-                    queue.append((col, row))
-                    visited[row][col] = True
-                    visited[col][row] = True
-                col += 1
-        k += 1
-        print("****")
-    print(f"groups: {groups}")
-    return len(groups) - 1
 
 
 matrix = \
