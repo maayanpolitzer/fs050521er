9e88725e43f043ba92d177268a07244463b0b820
maayanpolitzer <maayanp163@gmail.com>
2021-08-05 08:30:57 +0300

    4.8.2021
    update

--- fed/refresh/resources/script/myScripts.js
+++ fed/refresh/resources/script/myScripts.js
@@ -1,7 +1,6 @@
 var elad = true;
 
 function myFunction() {
-
     /*
     var redInput = document.getElementById("redInput");
     console.log(redInput.value);
@@ -13,14 +12,44 @@ function myFunction() {
     var red = getValueFromInputById("redInput");
     var green = getValueFromInputById("greenInput");
     var blue = getValueFromInputById("blueInput");
-    var color = "rgb(" + red + "," + green + "," + blue + ")";
+    //var color = "rgb(" + red + "," + green + "," + blue + ")"; // rgb(0,0,0)
+    var color = getColorFromRGB(red, green, blue);
     var preview = document.getElementById("preview");
     preview.style.backgroundColor = color;
 }
 
+function getColorFromRGB(red, green, blue) {
+    return "rgb(" + red + "," + green + "," + blue + ")"; // rgb(0,0,0)
+}
+
 function getValueFromInputById(elementId) {
-    var input = document.getElementById(elementId);
-    return input.value;
+    //var input = document.getElementById(elementId);
+    //return input.value;
+    return document.getElementById(elementId).value;
+}
+
+function addColor() {
+    // DO NOT CODE LIKE THIS!!! (I WILL KILL YOU)
+    //document.getElementById("colorList").innerHTML = document.getElementById("colorList").innerHTML + "<li style='background-color:pink' class='colorCircle'></li>";
+
+
+    //1. create circle.
+    // 1.1. create li tag (element):
+    var newColorElement = document.createElement("li");
+    // 1.2. add class that will change the style to circle:
+    newColorElement.classList.add("colorCircle");
+    //2. get the choosen color!
+    var choosenColor = document.getElementById("preview").style.backgroundColor;
+    //3. add color to the circle.
+    newColorElement.style.backgroundColor = choosenColor;
+    //4. add circle next to existing circles.
+    // 4.1. find the father tag/element.
+    var ul = document.getElementById("colorList");
+    // 4.2. add the new element to the father:
+    ul.appendChild(newColorElement);
+    //solution:
+    //add this "<li style="background-color:..." class="colorCircle"></li>" to the color list(ul).
+
 }
 
 function changePickerDivDisplay() {
@@ -37,7 +66,6 @@ function changePickerDivDisplay() {
         button.innerHTML = "Open Color Picker";
     }
     elad = !elad;
-
     /*
         var div = document.getElementById("pickerDiv");
         var button = document.getElementById("openPickerButton");
@@ -51,7 +79,6 @@ function changePickerDivDisplay() {
             button.innerHTML = "Open Color Picker";
         }
         */
-
 }
 
 
