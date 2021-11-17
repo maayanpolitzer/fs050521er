72f17b97f391fd75eeb51a6cc5c0a1598b569c7c
maayanpolitzer <maayanp163@gmail.com>
2021-11-17 21:21:05 +0200

    java
    17/11/2021

---
+++ java/firstProject/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    // one line comment
+    /*
+    multiline comment
+     */
+    // function in java is called: Method! מתודה
+    public static void main(String[] args) {
+        System.out.println("working!");
+    }
+
+}
+
+
